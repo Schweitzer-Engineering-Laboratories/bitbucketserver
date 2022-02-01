56d040b50b6132cb0c47e566d8a3af314ad12dcc
Jason Kemp <jason_kemp@selinc.com>
2022-01-31 18:57:17 -0800

    version bump to v1.1.0

--- bitbucketserver/__init__.py
+++ bitbucketserver/__init__.py
@@ -2,4 +2,4 @@ from bitbucketserver.bitbucketserver import BitbucketServer
 from bitbucketserver.connection import BitbucketServerException
 from bitbucketserver.utils import BitbucketJsonEncoder, translate_timestamp
 
-__version__ = '1.0.0' # change in setup.py as well!
+__version__ = '1.1.0' # change in setup.py as well!
